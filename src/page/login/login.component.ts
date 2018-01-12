fc8ed365f7cb092c9d7b7a40d2be52182dd33ce3
llkui <liukui178@163.com>
2018-01-12 15:35:32 +0800

    登录优化

--- src/page/login/login.component.ts
+++ src/page/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation}     from '@angular/core';
+import { Router }                          from '@angular/router';
 import { ToptipsService }                  from 'ngx-weui/toptips';
 import { ToastService }                    from 'ngx-weui/toast';
 
@@ -15,11 +16,13 @@ export class LoginComponent{
         username: string,
         password: string,
     }
+    loading: boolean = false;
 
     constructor(
         private toptips: ToptipsService,
         private toast: ToastService,
         private loginService: LoginService,
+        private router: Router,
     ) {}
 
     ngOnInit() {
@@ -38,18 +41,23 @@ export class LoginComponent{
             this.toptips.warn('密码不可为空');
             return;
         }
-        this.toast.show('登录中', 0, 'loading');
+        this.loading = true;
+        this.toast.loading('登录中', 100000);
         var params = {
             username: this.formData.username,
             password: this.formData.password,
         }
         this.loginService.adminlogin(params).then((data) => {
-            console.log(data);
+            this.toast.hide();
             if(data.status == 'no'){
                 this.toptips.warn(data.errorMsg);
             }else{
-
+                var results = JSON.parse(JSON.stringify(data.results));
+                sessionStorage.setItem('username', results.admininfo.username);
+                sessionStorage.setItem('token', results.admininfo.token);
+                this.router.navigate(['./user']);
             }
+            this.loading = false;
         });
     }
 }
